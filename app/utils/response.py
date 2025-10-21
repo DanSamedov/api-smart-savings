41ba0659e700e70fb5b3729ee385105be6fc2848
heisdanielade <heisdanielade@gmail.com>
2025-10-22 00:19:44 +0200

    update (minor): add app little app info to standard api response

--- app/utils/response.py
+++ app/utils/response.py
@@ -2,9 +2,14 @@
 
 from datetime import datetime, timezone
 
+from app.core.config import settings
+
+app_name = settings.APP_NAME
+
 
 def standard_response(status: str, message: str, data: dict = None):
     return {
+        "info": f"{app_name} API - v1",
         "timestamp": datetime.now(timezone.utc).isoformat(),
         "status": status,
         "message": message,
