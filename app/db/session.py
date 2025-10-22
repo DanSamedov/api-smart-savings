e9123c10f5a3ecbeb9cd7927d8f01068c1a5399b
heisdanielade <heisdanielade@gmail.com>
2025-10-22 23:57:33 +0200

    update: enfore UTC at the DB connection level

--- app/db/session.py
+++ app/db/session.py
@@ -2,9 +2,17 @@
 import os
 
 from sqlmodel import create_engine, Session
+from alchemy import event
+
 
 DATABASE_URL = os.getenv("DATABASE_URL", "")
 engine = create_engine(DATABASE_URL, echo=True)
+
+
+@event.listens_for(engine, "connect")
+def set_utc_timezone(dbapi_connection, connection_record):
+    with dbapi_connection.cursor() as cursor:
+        cursor.execute("SET TIMEZONE TO 'UTC';")
 
 
 def get_session():
