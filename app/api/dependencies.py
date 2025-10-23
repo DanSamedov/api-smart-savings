85efad89f2a6aa9c6cf80abe294f5e813137bdbc
heisdanielade <heisdanielade@gmail.com>
2025-10-23 22:45:20 +0200

    update: add authentication for API-docs routes
    - add dependency to authenticate credentials

---
+++ app/api/dependencies.py
@@ -0,0 +1,35 @@
+# app/api/dependencies.py
+
+import secrets
+
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import Depends, HTTPException, status
+from app.core.config import settings
+
+security = HTTPBasic()
+
+USERNAME = settings.DOCS_USERNAME
+PASSWORD = settings.DOCS_PASSWORD
+
+
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    """
+    Authenticate credentials for API-Docs URL.
+    """
+    if USERNAME is None or PASSWORD is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Configuration error, please contact dev team",
+        )
+
+    correct_username = secrets.compare_digest(credentials.username, USERNAME)
+    correct_password = secrets.compare_digest(credentials.password, PASSWORD)
+
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    return True
--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 # app/main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
+from fastapi.openapi.utils import get_openapi
 
 from .utils.response import standard_response
 from .core.logging import log_requests
+from .api.dependencies import authenticate
 
 app = FastAPI(
     title="SmartSave API",
@@ -37,3 +40,18 @@ def root():
         Dict[str, Any]: Standard response containing API status
     """
     return standard_response(status="success", message="API is live.")
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui(authenticated: bool = Depends(authenticate)):
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="API Docs")
+
+
+@app.get("/redoc", include_in_schema=False)
+async def custom_redoc_ui(authenticated: bool = Depends(authenticate)):
+    return get_redoc_html(openapi_url="/openapi.json", title="API Docs")
+
+
+@app.get("/openapi.json", include_in_schema=False)
+async def openapi_json(authenticated: bool = Depends(authenticate)):
+    return get_openapi(title="API Docs", version="1.0.0", routes=app.routes)
