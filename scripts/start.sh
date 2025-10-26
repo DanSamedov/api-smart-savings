31aa0b78cf9403ed164b583011d352d7de6f8c1d
heisdanielade <heisdanielade@gmail.com>
2025-10-27 00:36:34 +0100

    chore: add app startup script for dev and prod environments

---
+++ scripts/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "[START SCRIPT] (i) Waiting for database to be ready..."
+while ! pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER
+do
+  echo "[START SCRIPT] (i) Waiting for database connection..."
+  sleep 2
+done
+
+echo "[START SCRIPT] (i) Running database migrations..."
+alembic upgrade head
+
+echo "[START SCRIPT] (i) Starting FastAPI application..."
+uvicorn app.main:app --host 0.0.0.0 --port 3195 --reload
