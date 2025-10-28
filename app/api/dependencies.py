a04793ab174d226faceab19cb7acdd85e94c1cde
heisdanielade <heisdanielade@gmail.com>
2025-10-28 18:18:20 +0100

    update: add api dependency to get current user

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -3,7 +3,16 @@ import secrets
 
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import Session, select
+
 from app.core.config import settings
+from app.db.session import get_session
+from app.models.user_model import User
+from app.core.jwt import decode_token
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
 security = HTTPBasic()
@@ -19,7 +28,7 @@ def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
     if USERNAME is None or PASSWORD is None:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Configuration error, please contact dev team",
+            detail="Configuration error, please contact dev team.",
         )
 
     correct_username = secrets.compare_digest(credentials.username, USERNAME)
@@ -33,3 +42,28 @@ def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
         )
 
     return True
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), session: Session = Depends(get_session)
+) -> User:
+    payload = decode_token(token)
+    user_email = payload.get("sub")
+    if not user_email:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials.",
+        )
+    stmt = select(User).where(User.email == user_email)
+    user = session.exec(stmt).one_or_none()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="No account found with this email address"
+        )
+
+    if user.is_deleted:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Your account is scheduled for deletion.",
+        )
+    return user
