e8671c33659402ceb3f3b4848f321bd1aafb0d3b
heisdanielade <heisdanielade@gmail.com>
2025-11-04 21:02:55 +0100

    update: add helpers for location retrieval & time transformation for user-friendliness

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -2,6 +2,9 @@
 
 import hashlib
 import os
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
+import httpx
 
 SALT = os.getenv("IP_HASH_SALT")
 
@@ -24,3 +27,30 @@ def mask_email(email: str) -> str:
         return f"{masked_local}@{domain}"
     except Exception:
         return "****@****"
+
+
+def transform_time(time: datetime) -> str:
+    """Return a user-friendly time as string."""
+    local_dt = time.astimezone(ZoneInfo("Europe/London"))
+    transformed = local_dt.strftime("%b %d, %Y %I:%M %p %Z")
+    return transformed
+
+
+async def get_location_from_ip(ip: str) -> str:
+    """
+    Async IP geolocation lookup (non-blocking).
+    """
+    url = f"https://ipapi.co/{ip}/json/"
+    try:
+        async with httpx.AsyncClient(timeout=1.5) as client:
+            resp = await client.get(url)
+            data = resp.json()
+        city = data.get("city")
+        country = data.get("country_name")
+        if city and country:
+            return f"{city}, {country}"
+        elif country:
+            return country
+        return "Unknown location"
+    except Exception:
+        return "Unknown location"
