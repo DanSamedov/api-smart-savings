6b1d5c81d1823a4615701feea1dd86999bfac4f7
heisdanielade <heisdanielade@gmail.com>
2025-11-12 02:55:19 +0100

    chore (minor)

--- app/core/utils/response.py
+++ app/core/utils/response.py
@@ -6,11 +6,12 @@ from datetime import datetime, timezone
 from app.core.config import settings
 
 app_name = settings.APP_NAME
+app_version = settings.APP_VERSION
 
 
-def standard_response(message: Optional[str],status: str = "success", data: Optional[dict] = None) -> dict[str, Any]:
+def standard_response(message: Optional[str], status: str = "success", data: Optional[dict] = None) -> dict[str, Any]:
     return {
-        "info": f"{app_name} API - v1",
+        "info": f"{app_name} API - {app_version}",
         "timestamp": datetime.now(timezone.utc).isoformat(),
         "status": status,
         "message": message,
